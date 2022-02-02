ca550d5441495e972dd3f05cca701807d95db69b
NelliEva <55017487+NelliEva@users.noreply.github.com>
2022-02-02 12:31:24 -0700

    created react app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
 
 function App() {
+
+  const [visible, setVisible] = useState(false);
+  const [visible1, setVisible1] = useState(false);
+  const [visible2, setVisible2] = useState(false);
+  const [visible3, setVisible3] = useState(false);
+  const [visible4, setVisible4] = useState(false);
+
+  function increase() {
+    setTimeout(() => {
+      setVisible(true);
+    }, 2000);
+    setTimeout(() => {
+      setVisible1(true);
+    }, 4000);
+    setTimeout(() => {
+      setVisible2(true);
+    }, 6000);
+    setTimeout(() => {
+      setVisible3(true);
+    }, 8000);
+    setTimeout(() => {
+      setVisible4(true);
+    }, 10000);
+  }
+  function reset (){
+    setTimeout(() => {
+      setVisible4(false);
+    }, 2000);
+    setTimeout(() => {
+      setVisible3(false);
+    }, 4000);
+    setTimeout(() => {
+      setVisible2(false);
+    }, 6000);
+    setTimeout(() => {
+      setVisible1(false);
+    }, 8000);
+    setTimeout(() => {
+      setVisible(false);
+    }, 10000);
+
+  } 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="app">
+        <div className="scale">
+          <div className="sc"><p>80-</p></div>
+          <div className="sc"><p>60-</p></div>
+          <div className="sc"><p>40-</p></div>
+          <div className="sc"><p>20-</p></div>
+          <div className="sc" ><p style={{marginBottom:"0"}}> 0-</p></div>
+        </div>
+        <div className="bathtub">
+            { visible ?  <div className="water"></div> : null}
+            { visible1 ? <div className="water"></div> : null}
+            { visible2 ? <div className="water"></div> : null}
+            { visible3 ? <div className="water"></div> : null}
+            { visible4 ? <div className="water"></div> : null} 
+        </div>
+      </div>
+      <div className="btn">
+        <button onClick={() => increase()}>increaseWaterLevel</button>
+        <button onClick={() => reset()}>decreaseWaterLevel</button>
+      </div>
     </div>
   );
 }
